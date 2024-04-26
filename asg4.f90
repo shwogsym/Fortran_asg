7e2130b20df4f3735bac0fad07f430d53314a4e9
ogi <{megabit_fetch_0t@icloud.com}>
2024-04-26 18:08:58 +0900

    外部サブルーチン追加

--- asg4.f90
+++ asg4.f90
@@ -1,14 +1,34 @@
 program asg4 
     implicit none 
-    real(8) a,b,c,x1,x2,xm,fm
-    real(8) :: er = 1.0e-6 
-    integer :: t,max_t = 1000,fi = 10
+    real(8) a, b, c, x1, x2, xm
+    integer :: t, max_t = 1000
 
-    write(*,*) "Input a, b, c (ax^2 + bx + c = 0):" 
-    read(*,*) a, b, c 
+    a = 0.0d0
+    b = -2.0d0
+    c = 2.0d0
 
     write(*,*) "Input the start value x1 and end value x2"
     read(*,*) x1, x2 
+
+    call bisection_method(a, b, c, x1, x2, t, xm)
+
+    if (t == max_t) then
+        write(*,*) "No solution was found."
+    else
+        write(*,*) "Solution :", xm
+    end if
+    write(*,*) 'Number of repetitions:', t
+
+end program asg4
+
+
+!二分法の外部サブルーチン
+subroutine bisection_method(a, b, c, x1, x2, t, xm)
+    implicit none 
+    real(8) :: a, b, c, x1, x2, xm, fm
+    integer :: t, max_t = 1000, fi = 10
+    real(8), parameter :: er = 1.0e-6
+
 
     if (func(a, b, c, x1) * func(a, b, c, x2) > 0) then
         stop 'One of the values should be positive and the other negative.'
@@ -16,20 +36,22 @@ program asg4
     !２つの初期値が二分法を回す条件を満たしているのか検証
 
     t = 0 
-    open(fi,file = 'outasg4.d')
+    open(fi, file = 'outasg4.d')
 
-    do while (abs(x2-x1) > er .and. t < max_t) 
+    do while (abs(x2 - x1) > er .and. t < max_t) 
         xm = (x1 + x2) / 2.0
-        !中点値の計算
         fm = func(a, b, c, xm)
-        !中点を題意の関数に代入
-        if (fm < 0)  then
+        !中央値の計算
+
+        if (fm < 0) then
+             !中点の値が0以下のとき→x1,x2の中負の値の方と値を交換したい。↓で判定
             if (func(a, b, c, x2) < 0) then
                 x2 = xm
             else 
                 x1 = xm
             endif 
         else 
+            !else つまり、中点の値が0より大きいとき同様に判定
             if (func(a, b, c, x2) > 0) then
                 x2 = xm
             else 
@@ -37,18 +59,10 @@ program asg4
             endif 
         endif
         !中点の値に応じて初期値の入れ替えを行い、次のサイクルに準備
-     
-        t = t + 1 
-        !反復回数のカウント
-        write(fi,*)t,xm 
-    end do 
 
-    if (t == max_t) then
-        write(*,*)  "No solution was found."
-    else
-        write (*,*)  "Solution :", xm
-    end if
-    write (*,*) 'Number of repetition ;',t
+        t = t + 1 
+        write(fi,*) t, xm 
+    end do 
 
 contains 
 
@@ -57,9 +71,8 @@ contains
         real(8) :: a, b, c, x
         func = a*x**2.0d0 + b*x + c
     end function func
-    !関数の定義
 
-end program asg4
+end subroutine bisection_method
 
 
     
