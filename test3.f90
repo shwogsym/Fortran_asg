d7e67dbb1bde4a0284fc13c274f6ba6e7fbe53dc
ogi <{megabit_fetch_0t@icloud.com}>
2024-05-27 12:56:36 +0900

    Update

--- test3.f90
+++ test3.f90
@@ -1,66 +1,58 @@
+!データ修正の演算をここで定義
 module func_module
-    implicit none
-contains
-
-subroutine solution_formula(a,b,c,x1,x2)
-    implicit none 
-    real (8) a,b,c,x1,x2,D,sqrt_D
-    complex (8) cx1, cx2
-    integer ,parameter ::input_file = 10 ,output_file = 11
-    
-    D = b**2 - 4.0d0*a*c
-    
-    if (a == 0.0) then
-        if(b == 0.0) then 
-            stop "Invalid input a,b = 0"
-        else 
-            x1 = -c/b 
-            write(output_file,*) 'x = ',x1
-            write(*,*) 'x = ',x1
-        endif 
-    else 
-        if (D < 0) then            
-            sqrt_D = sqrt(-D)
-            cx1 = cmplx(-b/(2.0d0*a), sqrt_D/(2.0d0*a), kind=8)
-            cx2 = cmplx(-b/(2.0d0*a), -sqrt_D/(2.0d0*a), kind=8)
-            write (output_file,*) 'x1 =', cx1
-            write (output_file,*) 'x2 =', cx2
-            write (*,*) 'x1 =', cx1
-            write (*,*) 'x2 =', cx2
-        else
-            sqrt_D = sqrt(D)
-            if (b > 0) then
-                x1 = (-b - sqrt_D) / (2*a)
-                x2 = 2*c / (-b - sqrt_D)
-            else
-                x1 = 2*c / (-b + sqrt_D)
-                x2 = (-b + sqrt_D) / (2*a)
-            endif
-            write (output_file,*) 'x =',x1,x2
-            write (*,*) 'x =',x1,x2
-        endif
-    endif     
-    close(output_file)
-end subroutine solution_formula    
-end module func_module
-
-program asg1
+    implicit none  
+    contains
+ 
+    subroutine Fix_data(x)
+        implicit none 
+        real(8)x
+        x=x-0.01d0
+    end subroutine Fix_data
+ end module func_module
+ 
+ program read_data
     use func_module
     implicit none
-    real (8) a,b,c,x1,x2
-    integer :: io
-    integer ,parameter ::input_file = 10 ,output_file = 11
-
-    open (input_file ,file = 'asg1_file/inpasg1.dat',action = 'read',iostat = io)
-    if(io /= 0) stop 'Error : failed to open file'
-    open (output_file, file = 'asg1_file/outasg1.dat',action = 'write',iostat = io)
-    if(io /= 0) stop 'Error : failed to open file'
-
-    read(input_file,*) a,b,c !ファイルに二次関数の計数を入れておく (ax^2 + bx + c = 0)
-    close(input_file) 
-
-    call solution_formula(a,b,c,x1,x2) 
-    !解の係数公式の呼び出し　→　ファイル出力も含まれている
-    write (*,*) 'Output file was made in asg1_file'
-
-end program asg1
+   
+    integer :: i, j, io
+    integer, parameter :: max_rows = 100, max_cols = 100
+    integer :: size_lines, size_columns = 2, Fix_colum_number = 2 ! Size_colums→　データの行数を指定、Fix_colum_number →修正したいデータ列の番号 、データの行数については動的に決定される。
+    integer :: input_file_number = 10, output_file_number = 11
+    
+    real(8) :: line(max_cols) 
+    real(8), dimension(max_rows, max_cols) :: lines 
+ 
+    size_lines = 0
+  
+    open(input_file_number, file='asg2_file/inpasg2.dat', status='old', action='read',iostat = io)
+    if (io /= 0) stop 'Failure to open input file'
+    open(output_file_number, file='asg2_file/outasg2.dat', status='replace', action='write',iostat = io)
+    if (io /= 0) stop 'Failure to open output file'
+ 
+    !データの読み込み
+    do i = 1, max_rows
+        read(input_file_number, *, iostat=io) (line(j), j = 1, size_columns) !1行分のデータの読み込み
+        if(io /= 0) exit !データのデータ端を検出
+        size_lines = size_lines + 1 !データの行数をカウント
+ 
+        do j = 1, size_columns
+            lines(i, j) = line(j) !1行分　(line)のデータを linesに格納
+        end do
+    end do
+    close(input_file_number)
+ 
+    !Fix_colum_number 列目のデータについてデータの修正を行う
+    do i = 1,size_lines
+        call Fix_data(lines(i,Fix_colum_number))
+    enddo 
+ 
+    do i = 1, size_lines
+        do j = 1, size_columns
+            write(output_file_number, '(F24.16)', advance='no') lines(i, j)
+            ! advance = no →　出力時に改行しない
+        end do
+        write(output_file_number, *) !ファイル内での改行
+    end do
+    close(output_file_number)
+ 
+ end program read_data
