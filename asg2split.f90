41edce415e1e66178b161899c2261d98b927de07
ogi <{megabit_fetch_0t@icloud.com}>
2024-05-27 16:40:52 +0900

    Update

--- asg2split.f90
+++ asg2split.f90
@@ -19,17 +19,22 @@ module func_module
     
  end module func_module
  
+
+
+
  program read_data
     use func_module
     implicit none
    
     integer :: i, j, io
     integer, parameter :: max_rows = 100, max_cols = 100
-    integer :: size_lines, size_columns = 2, Fix_colum_number = 2 ! Size_colums→　データの行数を指定、Fix_colum_number →修正したいデータ列の番号 、データの行数については動的に決定される。
+    integer :: size_lines, size_columns = 2
     integer :: input_file_number = 10, output_file_number = 11
+
+     character(32) :: filename
     
     real(8) :: line(max_cols) 
-    real(8), dimension(max_rows, max_cols) :: lines 
+
  
     size_lines = 0
   
@@ -42,26 +47,17 @@ module func_module
     do i = 1, max_rows
         read(input_file_number, *, iostat=io) (line(j), j = 1, size_columns) !1行分のデータの読み込み
         if(io /= 0) exit !データのデータ端を検出
-        size_lines = size_lines + 1 !データの行数をカウント
- 
+
+        write(filename, '("asg2_file/"i2.2".dat")') i
+        open(i+output_file_number, file=filename, status = "replace",iostat=io)
+        if (io /= 0) stop 'Failure to open output file'
         do j = 1, size_columns
-            lines(i, j) = line(j) !1行分　(line)のデータを linesに格納
-        end do
-    end do
-    close(input_file_number)
+            call Fix_data(line(j))
+            write(i+output_file_number,'(F24.16)') line(j)
+        enddo
+        close(i+output_file_number)
+
+    enddo
  
-    !Fix_colum_number 列目のデータについてデータの修正を行う
-    do i = 1,size_lines
-        call Fix_data(lines(i,Fix_colum_number))
-    enddo 
- 
-    do i = 1, size_lines
-        do j = 1, size_columns
-            write(output_file_number, '(F24.16)', advance='no') lines(i, j)
-            ! advance = no →　出力時に改行しない
-        end do
-        write(output_file_number, *) !ファイル内での改行
-    end do
-    close(output_file_number)
- 
+
  end program read_data
